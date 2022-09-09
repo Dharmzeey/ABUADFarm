37ed0ad76e4a714e735a9cb1a63a528fc6709ec1
Dharmzeey <olanrewajuadam2020@gmail.com>
2022-09-09 01:05:27 +0100

    railway ready

--- Afarm/settings/base.py
+++ Afarm/settings/base.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
@@ -129,9 +130,9 @@ LOGIN_URL = "account_login"
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = "static/"
-STATICFILES_DIRS = [BASE_DIR / "static"]
-STATIC_ROOT = [BASE_DIR, 'staticfiles']
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 MEDIA_URL = "media/"
 MEDIA_ROOT = BASE_DIR / 'uploads/media/'
