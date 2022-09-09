897fb098e5beab903adc49479abee74231084c8b
Dharmzeey <olanrewajuadam2020@gmail.com>
2022-09-09 15:27:38 +0100

    heroku changed

--- Afarm/settings/production.py
+++ Afarm/settings/production.py
@@ -3,8 +3,8 @@ from .base import *
 DEBUG = True
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
-ALLOWED_HOSTS = ["abuadfarm-production.up.railway.app"]
-CSRF_TRUSTED_ORIGIN = ["https://abuadfarm-production.up.railway.app"]
+ALLOWED_HOSTS = ["abuadfarm-production.up.railway.app", "abuadfarm.herokuapp.com"]
+CSRF_TRUSTED_ORIGIN = ["https://abuadfarm-production.up.railway.app", "https://abuadfarm.herokuapp.com"]
 
 DATABASES = {
     # 'default': {
