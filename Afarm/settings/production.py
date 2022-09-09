9d59f7d570402337551fe9ef66505de7405590b5
Dharmzeey <olanrewajuadam2020@gmail.com>
2022-09-09 01:38:19 +0100

    railway ready

--- Afarm/settings/production.py
+++ Afarm/settings/production.py
@@ -2,3 +2,6 @@ from .base import *
 
 DEBUG = True
 SECRET_KEY = os.environ.get("SECRET_KEY")
+
+ALLOWED_HOST = ["abuadfarm-production.up.railway.app"]
+CSRF_TRUSTED_ORIGIN = ["https://abuadfarm-production.up.railway.app"]
