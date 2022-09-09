bb65b72969f8fb2eec182490a75ca730807c9d8d
Dharmzeey <olanrewajuadam2020@gmail.com>
2022-09-09 02:38:19 +0100

    railway ready

--- Afarm/settings/production.py
+++ Afarm/settings/production.py
@@ -5,3 +5,18 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 
 ALLOWED_HOSTS = ["abuadfarm-production.up.railway.app"]
 CSRF_TRUSTED_ORIGIN = ["https://abuadfarm-production.up.railway.app"]
+
+DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': '',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '',
+    }
+}
