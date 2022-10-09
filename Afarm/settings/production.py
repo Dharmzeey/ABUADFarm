adce9749961dc7f9131622248a6d75b1065e181e
Dharmzeey <olanrewajuadam2020@gmail.com>
2022-10-10 00:26:59 +0100

    database

--- Afarm/settings/production.py
+++ Afarm/settings/production.py
@@ -6,14 +6,20 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 ALLOWED_HOSTS = ["abuadfarm-production.up.railway.app", "abuadfarm.herokuapp.com"]
 CSRF_TRUSTED_ORIGIN = ["https://abuadfarm-production.up.railway.app", "https://abuadfarm.herokuapp.com"]
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': os.environ.get('PGDATABASE'),
+#         'USER':os.environ.get('PGUSER'),
+#         'PASSWORD':os.environ.get('PGPASSWORD'),
+#         'HOST':os.environ.get('PGHOST'),
+#         'PORT':os.environ.get('PGPORT')
+
+# }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('PGDATABASE'),
-        'USER':os.environ.get('PGUSER'),
-        'PASSWORD':os.environ.get('PGPASSWORD'),
-        'HOST':os.environ.get('PGHOST'),
-        'PORT':os.environ.get('PGPORT')
-
-}
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
